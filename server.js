ed642f05ddcce1489f43f2421733538cda0e7ef8
LoganneF <loganne.featherston@gmail.com>
2020-03-28 16:23:07 -0700

    added authorization routes and controllers to server.js. authorization is done

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const app = express()
 const mongoose = require("mongoose")
 const cors = require('cors')
 const PORT = 3000
+const session = require("express-session");
+const methodOverride = require("method-override");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "feedmeseymour", 
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(methodOverride("_method"));
 
 const gradesController = require("./controllers/grades")
 
@@ -41,6 +53,27 @@ app.use(express.json())
 
 // MOUNT ROUTE
 app.use("/grades", gradesController)
+
+//initial routes for authorization
+app.get("/app", (req, res) => {
+  if (req.session.currentUser) {
+    res.send("the party");
+  } else {
+    res.redirect("/sessions/new");
+  }
+});
+//initial routes for authorization
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    currentUser: req.session.currentUser
+  });
+});
+
+const usersController = require("./controllers/users.js");
+app.use("/users", usersController);
+
+const sessionsController = require("./controllers/sessions.js");
+app.use("/sessions", sessionsController);
 
 // PORT LISTENER
 app.listen(PORT, () => {
