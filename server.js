4c5be28efc3c6bffd1b8a27521e9fc2180768f13
LoganneF <loganne.featherston@gmail.com>
2020-03-29 19:40:55 -0700

    moved Chris's server.js in

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-const cors = require('cors')
+const cors = require("cors")
 const PORT = 3003
-const session = require("express-session");
-const methodOverride = require("method-override");
-
-app.use(express.urlencoded({ extended: false }));
+const session = require("express-session")
+const methodOverride = require("method-override")
+// const PORT = process.env.PORT
+app.use(express.urlencoded({ extended: false }))
 app.use(
-  session({
-    secret: "feedmeseymour", 
-    resave: false,
-    saveUninitialized: false
-  })
-);
-app.use(methodOverride("_method"));
-
+	session({
+		secret: "feedmeseymour",
+		resave: false,
+		saveUninitialized: false,
+	})
+)
+app.use(methodOverride("_method"))
 // MONGOOSE
 mongoose.connection.on("error", err =>
 	console.log(err.message + " is Mongod not running?")
@@ -28,11 +27,9 @@ mongoose.connect("mongodb://localhost:27017/auth", {
 mongoose.connection.once("open", () => {
 	console.log("connected to mongoose...")
 })
-
 // CORS middleware:
 // const whitelist = [
 //     "http://localhost:3003",
-//     "https://fathomless-sierra-68956.herokuapp.com"
 //   ];
 //   const corsOptions = {
 //     origin: function(origin, callback) {
@@ -43,44 +40,33 @@ mongoose.connection.once("open", () => {
 //       }
 //     }
 //   };
-  
-//   app.use(cors(corsOptions));
-
+// app.use(cors(corsOptions));
 // app.use(cors())
-
 // JSON
 app.use(express.json())
-
-const assignmentsController = require("./controllers/assignments.js")
-// MOUNT ROUTE
-app.use("/assignments", assignmentsController)
-
 //initial routes for authorization
 app.get("/app", (req, res) => {
-  if (req.session.currentUser) {
-    res.send("the party");
-  } else {
-    res.redirect("/sessions/new");
-  }
-});
-
+	if (req.session.currentUser) {
+		res.send("the party")
+	} else {
+		res.redirect("/sessions/new")
+	}
+})
 //initial routes for authorization
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    currentUser: req.session.currentUser
-  });
-});
-
+	res.render("index.ejs", {
+		currentUser: req.session.currentUser,
+	})
+})
 //CONTROLLER ROUTES
-const studentsController = require('./controllers/students.js')
-app.use('/students', studentsController)
-
-const usersController = require("./controllers/users.js");
-app.use("/users", usersController);
-
-const sessionsController = require("./controllers/sessions.js");
-app.use("/sessions", sessionsController);
-
+const assignmentsController = require("./controllers/assignments.js")
+app.use("/assignments", assignmentsController)
+const studentsController = require("./controllers/students.js")
+app.use("/students", studentsController)
+const usersController = require("./controllers/users.js")
+app.use("/users", usersController)
+const sessionsController = require("./controllers/sessions.js")
+app.use("/sessions", sessionsController)
 // PORT LISTENER
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`)
