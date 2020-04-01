99944dcc8456d83b9c884be4c094143c8d4db234
LoganneF <loganne.featherston@gmail.com>
2020-04-01 13:15:38 -0700

    changed studentSchema to overall grade instead of passing

--- models/student.js
+++ models/student.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const studentSchema = mongoose.Schema({
   name: {type: String, required: true},
-  passing: {type: Boolean, default: true}
+  grade: {type: Number, required: true}
 })
 
 module.exports = mongoose.model('Student', studentSchema)
