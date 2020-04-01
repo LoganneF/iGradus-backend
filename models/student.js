7c1912d20147c998f5ec7540b7129d784431e9bd
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-03-31 18:51:14 -0700

    backend modifications

--- models/student.js
+++ models/student.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const studentSchema = mongoose.Schema({
   name: {type: String, required: true},
+  studentImage: {type: String, default: "https://cdn.clipart.email/38ee7d3fbd4d64f0a95f3dc280ccecb0_gender-partitioned-babe_396-355.png"},
   passing: {type: Boolean, default: true}
 })
 
