6875b06f2450e724b9d0e0f5f56483410b3a01e7
Chris <chrissouth89@gmail.com>
2020-04-02 20:17:11 -0500

    model changes

--- models/assignment.js
+++ models/assignment.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 const assignmentSchema = mongoose.Schema({
-  date: {type: String, required: false},
   name: {type: String, required: true},
-  grade: {type: String, required: true}
+  date: {type: String, required: false},
+  description: {type: String, required: true},
+  avgrade: {type: String, required: true}
 })
 
 module.exports = mongoose.model('Assignment', assignmentSchema)
