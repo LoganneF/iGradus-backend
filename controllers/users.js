a55cec4e097714f5b3eef6c185b8a3d6071a2041
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-04-03 20:42:12 -0700

    latest updates

--- controllers/users.js
+++ controllers/users.js
@@ -7,13 +7,21 @@ router.get("/new", (req, res) => {
   res.render("users/new.ejs");
 });
 
+router.get('/', (req, res) => {
+  User.find({}, (err, foundUsers) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+    }
+    res.status(200).json(foundUsers)
+  })
+})
+
 router.post("/", (req, res) => {
   req.body.password = bcrypt.hashSync(
     req.body.password,
     bcrypt.genSaltSync(10)
   );
   User.create(req.body, (err, createdUser) => {
-    res.redirect("/");
   });
 });
 
