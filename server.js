9cddaf56c19c0331bb7a87a9b4a32354bdad5d9b
LoganneF <loganne.featherston@gmail.com>
2020-04-07 14:32:37 -0700

    took out old mongoose connection

--- server.js
+++ server.js
@@ -33,13 +33,7 @@ mongoose.connection.on("error", err =>
 	console.log(err.message + " is Mongod not running?")
 )
 mongoose.connection.on("disconnected", () => console.log("mongo disconnected"))
-mongoose.connect("mongodb://localhost:27017/auth", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-mongoose.connection.once("open", () => {
-	console.log("connected to mongoose...")
-})
+
 
 // CORS middleware:
 // const whitelist = [
